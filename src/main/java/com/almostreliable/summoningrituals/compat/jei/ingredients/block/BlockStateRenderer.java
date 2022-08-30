b004ec90c79a3482d2e06a813376e6d1a530d3f5
Relentless <relentless@rlnt.dev>
2022-08-30 21:10:00 +0200

    add block below properties tooltip rendering

--- src/main/java/com/almostreliable/summoningrituals/compat/jei/ingredients/block/BlockStateRenderer.java
+++ src/main/java/com/almostreliable/summoningrituals/compat/jei/ingredients/block/BlockStateRenderer.java
@@ -1,5 +1,7 @@
 package com.almostreliable.summoningrituals.compat.jei.ingredients.block;
 
+import com.almostreliable.summoningrituals.util.TextUtils;
+import com.google.common.collect.Maps;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Vector3f;
 import mezz.jei.api.ingredients.IIngredientRenderer;
@@ -13,22 +15,28 @@ import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.model.data.EmptyModelData;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BlockStateRenderer implements IIngredientRenderer<BlockState> {
 
     private final Minecraft mc;
     private final BlockRenderDispatcher blockRenderer;
     private final Player player;
+    private final Map<Integer, List<Component>> propertyTooltipCache;
     private final int size;
 
     public BlockStateRenderer(int size) {
         mc = Minecraft.getInstance();
         blockRenderer = mc.getBlockRenderer();
         this.player = mc.player;
+        propertyTooltipCache = new HashMap<>();
         this.size = size;
     }
 
@@ -59,7 +67,31 @@ public class BlockStateRenderer implements IIngredientRenderer<BlockState> {
     public List<Component> getTooltip(BlockState blockState, TooltipFlag tooltipFlag) {
         var stack = new ItemStack(blockState.getBlock());
         try {
-            return stack.getTooltipLines(player, tooltipFlag);
+            var tooltip = stack.getTooltipLines(player, tooltipFlag);
+            var stateId = Block.getId(blockState);
+            if (propertyTooltipCache.containsKey(stateId)) {
+                return propertyTooltipCache.get(stateId);
+            }
+
+            var defaultState = blockState.getBlock().defaultBlockState();
+            List<String> modifiedProps = new ArrayList<>();
+            for (var property : blockState.getProperties()) {
+                if (!blockState.getValue(property).equals(defaultState.getValue(property))) {
+                    modifiedProps.add("» " + property.getName() + ": " + blockState.getValue(property));
+                }
+            }
+
+            if (modifiedProps.isEmpty()) {
+                propertyTooltipCache.put(stateId, null);
+            } else {
+                tooltip.add(TextComponent.EMPTY);
+                tooltip.add(TextUtils.translate("tooltip", "relevant_properties", ChatFormatting.AQUA));
+                for (var prop : modifiedProps) {
+                    tooltip.add(new TextComponent(prop));
+                }
+                propertyTooltipCache.put(stateId, tooltip);
+            }
+            return tooltip;
         } catch (RuntimeException | LinkageError e) {
             return List.of(new TextComponent("Error rendering tooltip!").append(e.getMessage())
                 .withStyle(ChatFormatting.DARK_RED));
