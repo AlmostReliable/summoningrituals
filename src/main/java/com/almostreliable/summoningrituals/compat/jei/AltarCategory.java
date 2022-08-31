e8ca0898c6b28443aad93336ef2d0616906292bb
Relentless <relentless@rlnt.dev>
2022-08-31 17:35:21 +0200

    render item inputs and implement block below offset

--- src/main/java/com/almostreliable/summoningrituals/compat/jei/AltarCategory.java
+++ src/main/java/com/almostreliable/summoningrituals/compat/jei/AltarCategory.java
@@ -20,6 +20,9 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.almostreliable.summoningrituals.Constants.ALTAR;
 import static com.almostreliable.summoningrituals.util.TextUtils.f;
 
@@ -29,6 +32,11 @@ public class AltarCategory implements IRecipeCategory<AltarRecipe> {
     private static final ResourceLocation TEXTURE = TextUtils.getRL(f("textures/jei/{}.png", ALTAR));
     private static final int TEXTURE_WIDTH = 150;
     private static final int TEXTURE_HEIGHT = 157;
+    private static final int CENTER_X = TEXTURE_WIDTH / 2;
+    private static final int RENDER_Y = 50;
+    private static final int RENDER_SIZE = 24;
+    private static final int ITEM_RADIUS = 45;
+    private static final int MOB_RADIUS = 65;
 
     private final IDrawable background;
     private final IDrawable icon;
@@ -37,13 +45,14 @@ public class AltarCategory implements IRecipeCategory<AltarRecipe> {
     private final SizedItemRenderer sizedItemRenderer;
 
     public AltarCategory(IGuiHelper guiHelper) {
-        background = guiHelper.drawableBuilder(TEXTURE, 0, 0, TEXTURE_WIDTH, TEXTURE_HEIGHT)
-            .setTextureSize(TEXTURE_WIDTH, TEXTURE_HEIGHT)
-            .build();
+        // background = guiHelper.drawableBuilder(TEXTURE, 0, 0, TEXTURE_WIDTH, TEXTURE_HEIGHT)
+        //     .setTextureSize(TEXTURE_WIDTH, TEXTURE_HEIGHT)
+        //     .build();
+        background = guiHelper.createBlankDrawable(TEXTURE_WIDTH, TEXTURE_HEIGHT);
         icon = guiHelper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(Setup.ALTAR_ITEM.get()));
         // row = guiHelper.drawableBuilder(TEXTURE, 0, 0, 146, 18).setTextureSize(146, 38).build();
-        blockStateRenderer = new BlockStateRenderer(32);
-        sizedItemRenderer = new SizedItemRenderer(32);
+        blockStateRenderer = new BlockStateRenderer(RENDER_SIZE);
+        sizedItemRenderer = new SizedItemRenderer(RENDER_SIZE);
     }
 
     @Override
@@ -68,15 +77,33 @@ public class AltarCategory implements IRecipeCategory<AltarRecipe> {
 
     @Override
     public void setRecipe(IRecipeLayoutBuilder builder, AltarRecipe recipe, IFocusGroup focuses) {
-        // TODO: shift altar upwards if there is a block below
+        var yOffset = blockStateRenderer.getHeight() / 2;
         if (recipe.getBlockBelow() != null) {
-            builder.addSlot(RecipeIngredientRole.RENDER_ONLY, (TEXTURE_WIDTH - sizedItemRenderer.getWidth()) / 2, 55)
+            builder.addSlot(RecipeIngredientRole.RENDER_ONLY, CENTER_X - blockStateRenderer.getWidth() / 2, RENDER_Y + blockStateRenderer.getHeight())
                 .setCustomRenderer(AlmostTypes.BLOCK_STATE, blockStateRenderer)
                 .addIngredient(AlmostTypes.BLOCK_STATE, recipe.getBlockBelow().getDisplayState());
+            yOffset = 0;
         }
-        builder.addSlot(RecipeIngredientRole.RENDER_ONLY, (TEXTURE_WIDTH - sizedItemRenderer.getWidth()) / 2, 35)
+        builder.addSlot(RecipeIngredientRole.RENDER_ONLY, CENTER_X - sizedItemRenderer.getWidth() / 2, RENDER_Y + yOffset)
             .setCustomRenderer(VanillaTypes.ITEM_STACK, sizedItemRenderer)
             .addItemStack(new ItemStack(Setup.ALTAR_ITEM.get()));
+        builder.addSlot(RecipeIngredientRole.INPUT, CENTER_X - 8, RENDER_Y - 16 + yOffset)
+            .addIngredients(recipe.getCatalyst());
+
+        var inputs = recipe.getInputs();
+        var inputSize = inputs.size();
+        for (var i = 0; i < inputSize; i++) {
+            var x = CENTER_X + (int) (Math.cos(i * 2 * Math.PI / inputSize) * ITEM_RADIUS) - 8;
+            var y = RENDER_Y + (int) (Math.sin(i * 2 * Math.PI / inputSize) * ITEM_RADIUS) + 8;
+
+            List<ItemStack> inputStacks = new ArrayList<>();
+            for (var stack : inputs.get(i).ingredient().getItems()) {
+                stack.setCount(inputs.get(i).count());
+                inputStacks.add(stack);
+            }
+
+            builder.addSlot(RecipeIngredientRole.INPUT, x, y).addItemStacks(inputStacks);
+        }
     }
 
     @Override
