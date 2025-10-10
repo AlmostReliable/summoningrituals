94b173b839f771beefec73f7e28636d3cacb13e6
rlnt <relentless@rlnt.dev>
2025-10-10 12:47:02 +0200

    expose weather and location loot condition builders directly

--- src/main/java/com/almostreliable/summoningrituals/compat/kubejs/builder/StartConditionsBuilder.java
+++ src/main/java/com/almostreliable/summoningrituals/compat/kubejs/builder/StartConditionsBuilder.java
@@ -1,5 +1,7 @@
 package com.almostreliable.summoningrituals.compat.kubejs.builder;
 
+import net.minecraft.advancements.critereon.LocationPredicate;
+import net.minecraft.world.level.storage.loot.predicates.LocationCheck;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraft.world.level.storage.loot.predicates.WeatherCheck;
 
@@ -7,23 +9,23 @@ import com.google.common.base.Preconditions;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 public class StartConditionsBuilder {
 
     private final List<LootItemCondition> conditions = new ArrayList<>();
 
-    public StartConditionsBuilder weatherRaining() {
-        conditions.add(WeatherCheck.weather().setRaining(true).build());
+    public StartConditionsBuilder weather(Function<WeatherCheck.Builder, WeatherCheck.Builder> weather) {
+        var builder = WeatherCheck.weather();
+        var weatherCheck = weather.apply(builder).build();
+        conditions.add(weatherCheck);
         return this;
     }
 
-    public StartConditionsBuilder weatherThundering() {
-        conditions.add(WeatherCheck.weather().setThundering(true).build());
-        return this;
-    }
-
-    public StartConditionsBuilder weatherClear() {
-        conditions.add(WeatherCheck.weather().setRaining(false).setThundering(false).build());
+    public StartConditionsBuilder location(Function<LocationPredicate.Builder, LocationPredicate.Builder> location) {
+        var predicateBuilder = LocationPredicate.Builder.location();
+        var locationCheck = LocationCheck.checkLocation(location.apply(predicateBuilder)).build();
+        conditions.add(locationCheck);
         return this;
     }
 
