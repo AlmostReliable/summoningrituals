616128f2bc55f92787df0a96b5a69dc594e2d94a
rlnt <relentless@rlnt.dev>
2025-10-26 15:02:19 +0100

    update almost gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("net.neoforged.moddev") version "2.0.80"
-    id("com.almostreliable.almostgradle") version "1.3.0"
+    id("com.almostreliable.almostgradle") version "1.4.1"
 }
 
 almostgradle.setup {
