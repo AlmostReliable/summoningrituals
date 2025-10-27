a3ed5ca078b35dbb9644645fc87c7201fadfb989
rlnt <relentless@rlnt.dev>
2025-10-27 12:37:16 +0100

    add time check condition to condition builder

--- src/main/java/com/almostreliable/summoningrituals/compat/kubejs/builder/StartConditionsBuilder.java
+++ src/main/java/com/almostreliable/summoningrituals/compat/kubejs/builder/StartConditionsBuilder.java
@@ -1,19 +1,33 @@
 package com.almostreliable.summoningrituals.compat.kubejs.builder;
 
 import net.minecraft.advancements.critereon.LocationPredicate;
+import net.minecraft.world.level.storage.loot.IntRange;
 import net.minecraft.world.level.storage.loot.predicates.LocationCheck;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraft.world.level.storage.loot.predicates.TimeCheck;
 import net.minecraft.world.level.storage.loot.predicates.WeatherCheck;
 
 import com.google.common.base.Preconditions;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 
+@SuppressWarnings("unused")
 public class StartConditionsBuilder {
 
     private final List<LootItemCondition> conditions = new ArrayList<>();
+
+    public StartConditionsBuilder time(int min, int max) {
+        conditions.add(new TimeCheck(Optional.empty(), IntRange.range(min, max)));
+        return this;
+    }
+
+    public StartConditionsBuilder time(TimeTypes timeType) {
+        conditions.add(new TimeCheck(Optional.empty(), timeType.range));
+        return this;
+    }
 
     public StartConditionsBuilder weather(Function<WeatherCheck.Builder, WeatherCheck.Builder> weather) {
         var builder = WeatherCheck.weather();
@@ -35,5 +49,21 @@ public class StartConditionsBuilder {
             "only one weather condition is allowed"
         );
         return conditions;
+    }
+
+    public enum TimeTypes {
+        DAY(0, 12_000),
+        NIGHT(12_000, 24_000),
+        MORNING(0, 4_000),
+        NOON(4_000, 8_000),
+        AFTERNOON(8_000, 10_000),
+        EVENING(10_000, 12_000),
+        MIDNIGHT(17_000, 19_000);
+
+        private final IntRange range;
+
+        TimeTypes(int min, int max) {
+            this.range = IntRange.range(min, max);
+        }
     }
 }
