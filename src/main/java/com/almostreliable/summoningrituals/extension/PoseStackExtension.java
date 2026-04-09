12017d8973e70c51f5fe2de42838d78c9fc3d959
rlnt <relentless@rlnt.dev>
2026-04-09 14:47:43 +0200

    remove unused extension interface

--- src/main/java/com/almostreliable/summoningrituals/extension/PoseStackExtension.java
+++
@@ -1,8 +0,0 @@
-package com.almostreliable.summoningrituals.extension;
-
-public interface PoseStackExtension {
-
-    default void summoning$translate(float translate) {}
-
-    default void summoning$scale(float scale) {}
-}
--- src/main/java/com/almostreliable/summoningrituals/extension/package-info.java
+++
@@ -1,4 +0,0 @@
-@NonnullDefault
-package com.almostreliable.summoningrituals.extension;
-
-import org.lwjgl.system.NonnullDefault;
