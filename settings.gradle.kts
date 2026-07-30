69b042866fadb5c645108dbe219562312f956407
rlnt <16513358+rlnt@users.noreply.github.com>
2026-07-30 11:33:58 +0200

    remove foojay resolver plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,3 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
-}
-
 val modName: String by extra
 val minecraftVersion: String by extra
 rootProject.name = "${modName.replace(" ", "-")}-$minecraftVersion-NeoForge"
